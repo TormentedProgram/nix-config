52f55f5b4126c02b6b77f6ea84aeb17c4c76865c
TormentedProgram <glitchglitch32@gmail.com>
2025-09-08 17:26:07 +1000

    Update browser.nix

---
+++ modules/home/browser.nix
@@ -0,0 +1,66 @@
+{ pkgs, ... }:
+{
+  # Nix packages to install to $HOME
+  #
+  # Search for packages here: https://search.nixos.org/packages
+  home.packages = with pkgs; [];
+
+  # Programs natively supported by home-manager.
+  # They can be configured in `programs.*` instead of using home.packages.
+  programs = {
+    librewolf = {
+      enable = true;
+      profiles.default = {
+        extensions.packages = with pkgs.nur.rycee.firefox-addons; [
+          ublock-origin
+          sponsorblock
+          darkreader
+        ];
+        id = 0;
+        settings = {
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+
+          "extensions.autoDisableScopes" = 0;
+
+          "browser.uidensity" = 1;
+          "browser.search.openintab" = true;
+          "xpinstall.signatures.required" = false;
+          "extensions.update.enabled" = false;
+
+          "browser.display.use_document_fonts" = true;
+          "media.videocontrols.picture-in-picture.enabled" = true;
+
+          "widget.non-native-theme.enabled" = false;
+
+          "browser.newtabpage.enabled" = false;
+          "browser.startup.homepage" = "about:blank";
+
+          "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+          "browser.newtabpage.activity-stream.telemetry" = false;
+          "browser.ping-centre.telemetry" = false;
+          "toolkit.telemetry.archive.enabled" = false;
+          "toolkit.telemetry.bhrPing.enabled" = false;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.firstShutdownPing.enabled" = false;
+          "toolkit.telemetry.hybridContent.enabled" = false;
+          "toolkit.telemetry.newProfilePing.enabled" = false;
+          "toolkit.telemetry.reportingpolicy.firstRun" = false;
+          "toolkit.telemetry.shutdownPingSender.enabled" = false;
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.updatePing.enabled" = false;
+
+          "experiments.activeExperiment" = false;
+          "experiments.enabled" = false;
+          "experiments.supported" = false;
+          "network.allow-experiments" = false;
+
+          "privacy.clearOnShutdown_v2.cookiesAndStorage" = false;
+        };
+      };
+    };
+  };
+
+  # Services natively supported by home-manager.
+  # They can be configured in `services.*` instead of using home.packages.
+  services = {};
+}
