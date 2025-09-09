fe8f9f7708e1d6f0ec18f7bc4f10ec23c353217a
T0r <69302186+TormentedProgram@users.noreply.github.com>
2025-09-09 07:34:47 +0000

    Create peripheral.nix

---
+++ modules/home/peripheral.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+{
+  # Nix packages to install to $HOME
+  #
+  # Search for packages here: https://search.nixos.org/packages
+  home.packages = with pkgs; [
+    headsetcontrol
+  ];
+
+  # Programs natively supported by home-manager.
+  # They can be configured in `programs.*` instead of using home.packages.
+  programs = {};
+
+  # Services natively supported by home-manager.
+  # They can be configured in `services.*` instead of using home.packages.
+  services = {};
+}
