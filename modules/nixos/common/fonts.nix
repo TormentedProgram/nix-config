1329f204a406fc77f9cfdff67f79d67b6dd884c1
T0r <69302186+TormentedProgram@users.noreply.github.com>
2025-09-09 05:31:05 +0000

    Create fonts.nix

---
+++ modules/nixos/common/fonts.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+{
+  fonts = {
+    fontDir.enable = true;
+    fontconfig.useEmbeddedBitmaps = true;
+    packages = with pkgs; [
+      ubuntu_font_family
+      liberation_ttf
+      noto-fonts
+      fira-code
+      noto-fonts-cjk-sans
+      jetbrains-mono
+      nerd-fonts.iosevka
+      nerd-fonts.jetbrains-mono # unstable
+      nerd-fonts.fira-code # unstable
+      nerd-fonts.fantasque-sans-mono #unstable
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+    ];
+
+    fontconfig = {
+      defaultFonts = {
+        serif = [  "Liberation Serif" "Noto Serif CJK JP" "Noto Serif CJK KR" "Noto Serif CJK SC" "Noto Serif CJK TC" "Noto Serif CJK HK" ];
+        sansSerif = [ "Ubuntu"" Noto Sans CJK JP" "Noto Sans CJK KR" "Noto Sans CJK SC" "Noto Sans CJK TC" "Noto Sans CJK HK" ];
+        monospace = [ "Ubuntu Mono" ];
+      };
+    };
+  };
+};
