e5bb2440b5447ad7b9bdd7b4fde17ea26e7317b8
T0r <69302186+TormentedProgram@users.noreply.github.com>
2025-09-09 04:59:50 +0000

    Update sudo.nix

--- modules/nixos/common/sudo.nix
+++ modules/nixos/common/sudo.nix
@@ -3,4 +3,33 @@
   environment.systemPackages = with pkgs; [
     hyprpolkitagent
   ];
+
+  security.sudo = 
+  let
+    inherit (lib) mkForce mkDefault getExe';
+  in {
+    enable = true;
+    extraConfig = ''
+      Defaults lecture = never
+      Defaults pwfeedback
+    '';
+
+    extraRules = [
+      {
+        groups = [ "wheel" ];
+
+        commands = [
+          # allow reboot and shutdown without password
+          {
+            command = getExe' pkgs.systemd "reboot";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = getExe' pkgs.systemd "shutdown";
+            options = [ "NOPASSWD" ];
+          }
+        ];
+      }
+    ];
+  };
 }
