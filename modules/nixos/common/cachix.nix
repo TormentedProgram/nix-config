b07df9ffb89cc382345183e944762861229d5aff
T0r <69302186+TormentedProgram@users.noreply.github.com>
2025-09-09 16:22:01 +0000

    Create cachix.nix

---
+++ modules/nixos/common/cachix.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+{
+  nix.settings = {
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org" # For common packages like starship
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];  
+  };
+}
