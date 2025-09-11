65a645883881a34b29ca830b1eaff91fdabd49f2
T0r <69302186+TormentedProgram@users.noreply.github.com>
2025-09-11 18:58:03 +0000

    Update auto_defaults.nix

--- modules/nixos/common/auto_defaults.nix
+++ modules/nixos/common/auto_defaults.nix
@@ -11,6 +11,19 @@ in
       "x-scheme-handler/https" = "${defaults.browser}.desktop";
       "x-scheme-handler/about" = "${defaults.browser}.desktop";
       "x-scheme-handler/unknown" = "${defaults.browser}.desktop";
+
+      "image/png" = "${defaults.imageViewer}.desktop";
+      "image/jpeg" = "${defaults.imageViewer}.desktop";
+      "image/webp" = "${defaults.imageViewer}.desktop";
+
+      "video/mp4" = "${defaults.videoPlayer}.desktop";
+      "video/webm" = "${defaults.videoPlayer}.desktop";
+      "video/x-matroska" = "${defaults.videoPlayer}.desktop";
+      "video/quicktime" = "${defaults.videoPlayer}.desktop"; 
+
+      "audio/mpeg" = "${defaults.videoPlayer}.desktop"; # please don't make me do all the mimetypes for mp3s
+      "audio/wav" = "${defaults.videoPlayer}.desktop";
+      "audio/flac" = "${defaults.videoPlayer}.desktop";
     };
   };
 
