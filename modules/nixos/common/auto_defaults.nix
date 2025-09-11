d78a926a171f238bbb2c3e510eaf2b7ea6588657
T0r <69302186+TormentedProgram@users.noreply.github.com>
2025-09-11 19:00:31 +0000

    Update auto_defaults.nix

--- modules/nixos/common/auto_defaults.nix
+++ modules/nixos/common/auto_defaults.nix
@@ -6,21 +6,25 @@ in
   xdg.mime = {
     enable = true;
     defaultApplications = {
+      # BROWSER
       "text/html" = "${defaults.browser}.desktop";
       "x-scheme-handler/http" = "${defaults.browser}.desktop";
       "x-scheme-handler/https" = "${defaults.browser}.desktop";
       "x-scheme-handler/about" = "${defaults.browser}.desktop";
       "x-scheme-handler/unknown" = "${defaults.browser}.desktop";
 
+      # IMAGES
       "image/png" = "${defaults.imageViewer}.desktop";
       "image/jpeg" = "${defaults.imageViewer}.desktop";
       "image/webp" = "${defaults.imageViewer}.desktop";
 
+      # VIDEO
       "video/mp4" = "${defaults.videoPlayer}.desktop";
       "video/webm" = "${defaults.videoPlayer}.desktop";
       "video/x-matroska" = "${defaults.videoPlayer}.desktop";
       "video/quicktime" = "${defaults.videoPlayer}.desktop"; 
 
+      # AUDIO
       "audio/mpeg" = "${defaults.videoPlayer}.desktop"; # please don't make me do all the mimetypes for mp3s
       "audio/wav" = "${defaults.videoPlayer}.desktop";
       "audio/flac" = "${defaults.videoPlayer}.desktop";
